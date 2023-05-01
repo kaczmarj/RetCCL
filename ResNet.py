29584d4ec5759c9b5cd2435b4b61a9ff44ee0b78
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-05-01 00:42:06 -0400

    comment out import of load_state_dict_from_url

--- ResNet.py
+++ ResNet.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+# from torchvision.models.utils import load_state_dict_from_url
 import torch.nn.functional as F
 from torch.nn import Parameter
 
